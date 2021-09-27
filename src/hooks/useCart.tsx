9366ed86dc699eb53d4df50c8bf3ff361631af09
Rafinhaa <rafinhaa@me.com>
2021-09-27 11:54:15 -0300

    userCart method addProduct test passed

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,41 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const newCart = [...cart];
+      const productExists = newCart.find(product => productId === product.id);      
+      const stock = await api.get(`stock/${productId}`).then(response => response.data);
+      let amount = productExists ? productExists.amount : 1;
+      
+      if (!amount > stock.amount ){
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if(productExists){
+        productExists.amount++; // As alterações refletem automaticamente no produto dentro do newCart
+      }else{
+        const product = await api.get(`products/${productId}`).then(response => response.data);
+        const newProduct = {
+          ...product, // O novo produto terá dos os campos do product 
+          amount: amount, // E terá também o campo amount, porque o carrinho é do tipo Product
+        }
+        newCart.push(newProduct)
+      }
+      setCart(newCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
